f1115c2721eb793f81f239af5f4c545816b307b5
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-01 20:45:21 +0000

    Visually separate ticket routes

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,6 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+// Ticket Routes...
 Route::resource('tickets', 'TicketController');
 Route::resource('tickets/{ticket}/posts', 'TicketPostController');
