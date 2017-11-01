529fa923a65e7542268380e0dc5e091db3988798
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-01 19:37:39 +0000

    Implement ticket/post factories

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Ticket::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function () {
+            return factory(App\Models\User::class)->create()->id;
+        },
+        'summary' => $faker->sentence(),
     ];
 });
