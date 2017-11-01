05742519ba186c099abee47ec1ad68203d30103b
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-01 00:35:57 +0000

    Add registration and authentication routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::get('/', 'HomeController@index')->name('home');
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
