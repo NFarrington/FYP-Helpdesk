33d7b86fafbd417d814805a2c0b2851c9fd640fa
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-01 20:42:57 +0000

    Add dummy ticket policies

---
+++ app/Policies/TicketPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TicketPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the ticket.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Ticket  $ticket
+     * @return mixed
+     */
+    public function view(User $user, Ticket $ticket)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create tickets.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the ticket.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Ticket  $ticket
+     * @return mixed
+     */
+    public function update(User $user, Ticket $ticket)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the ticket.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Ticket  $ticket
+     * @return mixed
+     */
+    public function delete(User $user, Ticket $ticket)
+    {
+        return true;
+    }
+}
