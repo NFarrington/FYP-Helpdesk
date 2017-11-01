727a3104ace1f68275971318002d5dad2d1e5bf9
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-01 21:03:44 +0000

    Comment/remove unused policies

--- app/Policies/TicketPostPolicy.php
+++ app/Policies/TicketPostPolicy.php
@@ -10,50 +10,27 @@ class TicketPostPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the ticketPost.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TicketPost  $ticketPost
-     * @return mixed
-     */
-    public function view(User $user, TicketPost $ticketPost)
-    {
-        return true;
-    }
+//    /**
+//     * Determine whether the user can update the ticketPost.
+//     *
+//     * @param  \App\Models\User  $user
+//     * @param  \App\Models\TicketPost  $ticketPost
+//     * @return mixed
+//     */
+//    public function update(User $user, TicketPost $ticketPost)
+//    {
+//        return true;
+//    }
 
-    /**
-     * Determine whether the user can create ticketPosts.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        return true;
-    }
-
-    /**
-     * Determine whether the user can update the ticketPost.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TicketPost  $ticketPost
-     * @return mixed
-     */
-    public function update(User $user, TicketPost $ticketPost)
-    {
-        return true;
-    }
-
-    /**
-     * Determine whether the user can delete the ticketPost.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TicketPost  $ticketPost
-     * @return mixed
-     */
-    public function delete(User $user, TicketPost $ticketPost)
-    {
-        return true;
-    }
+//    /**
+//     * Determine whether the user can delete the ticketPost.
+//     *
+//     * @param  \App\Models\User  $user
+//     * @param  \App\Models\TicketPost  $ticketPost
+//     * @return mixed
+//     */
+//    public function delete(User $user, TicketPost $ticketPost)
+//    {
+//        return true;
+//    }
 }
