ae1c74ed6e62c73597bbccdd0cdaa9801e946d35
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-01 18:01:11 +0000

    Implement ticket and post migrations

--- database/migrations/2017_11_01_163531_create_tickets_table.php
+++ database/migrations/2017_11_01_163531_create_tickets_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateTicketsTable extends Migration
 {
@@ -15,7 +15,11 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('summary', 250);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
--- database/migrations/2017_11_01_170510_create_ticket_posts_table.php
+++ database/migrations/2017_11_01_170510_create_ticket_posts_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateTicketPostsTable extends Migration
 {
@@ -15,7 +15,13 @@ class CreateTicketPostsTable extends Migration
     {
         Schema::create('ticket_posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('ticket_id');
+            $table->unsignedInteger('user_id');
+            $table->text('content');
             $table->timestamps();
+
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
