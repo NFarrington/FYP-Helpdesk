439acfb6d76ab91b07ada388b7b806b163d389df
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-03 15:46:59 +0000

    Update ticket factories

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -4,9 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Ticket::class, function (Faker $faker) {
     return [
-        'user_id' => function () {
-            return factory(App\Models\User::class)->create()->id;
-        },
+        'status_id' => DB::table('ticket_statuses')->inRandomOrder()->first()->id,
+        'user_id' => factory(App\Models\User::class)->create()->id,
         'summary' => $faker->sentence(),
     ];
 });
---
+++ database/factories/TicketStatusFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\TicketStatus;
+use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
+
+$factory->define(TicketStatus::class, function (Faker $faker) {
+    return [
+        'name' => "Random - {$faker->word}",
+        'state' => Arr::random([
+            TicketStatus::STATUS_AGENT,
+            TicketStatus::STATUS_CUSTOMER,
+            TicketStatus::STATUS_CLOSED
+        ]),
+    ];
+});
+
+$factory->state(TicketStatus::class, 'agent',  function (Faker $faker) {
+    return [
+        'name' => "Agent - {$faker->word}",
+        'state' => TicketStatus::STATUS_AGENT,
+    ];
+});
+
+$factory->state(TicketStatus::class, 'customer',  function (Faker $faker) {
+    return [
+        'name' => "Customer - {$faker->word}",
+        'state' => TicketStatus::STATUS_CUSTOMER,
+    ];
+});
+
+$factory->state(TicketStatus::class, 'open',  function (Faker $faker) {
+    return [
+        'name' => "Open - {$faker->word}",
+        'state' => Arr::random([
+            TicketStatus::STATUS_AGENT,
+            TicketStatus::STATUS_CUSTOMER
+        ]),
+    ];
+});
+
+$factory->state(TicketStatus::class, 'closed',  function (Faker $faker) {
+    return [
+        'name' => "Closed - {$faker->word}",
+        'state' => TicketStatus::STATUS_CLOSED,
+    ];
+});
