cbd22e9d31eb774e9a66d58149f78d5a8eb752f0
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-05 14:48:03 +0000

    Add auth middleware to controllers

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -11,6 +11,16 @@ use Illuminate\Http\Request;
 class TicketController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @codeCoverageIgnore
