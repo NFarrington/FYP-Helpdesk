38a9bf4a1951ca8d15bdb4db105868583e8a266e
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-05 15:12:36 +0000

    Add scopes to ticket factory

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -1,13 +1,38 @@
 <?php
 
+use App\Models\Ticket;
 use Faker\Generator as Faker;
 
-$factory->define(\App\Models\Ticket::class, function (Faker $faker) {
+$factory->define(Ticket::class, function (Faker $faker) {
     return [
         'status_id' => function () {
-            return DB::table('ticket_statuses')->inRandomOrder()->first()->id;
+            return \App\Models\TicketStatus::inRandomOrder()->first()->id;
         },
         'user_id' => factory(App\Models\User::class)->create()->id,
         'summary' => $faker->sentence(),
+    ];
+});
+
+$factory->state(Ticket::class, 'agent', function (Faker $faker) {
+    return [
+        'status_id' => \App\Models\TicketStatus::withAgent()->first()->id,
+    ];
+});
+
+$factory->state(Ticket::class, 'customer', function (Faker $faker) {
+    return [
+        'status_id' => \App\Models\TicketStatus::withCustomer()->first()->id,
+    ];
+});
+
+$factory->state(Ticket::class, 'open', function (Faker $faker) {
+    return [
+        'status_id' => \App\Models\TicketStatus::open()->first()->id,
+    ];
+});
+
+$factory->state(Ticket::class, 'closed', function (Faker $faker) {
+    return [
+        'status_id' => \App\Models\TicketStatus::closed()->first()->id,
     ];
 });
