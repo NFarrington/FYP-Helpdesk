dcf658c2a86d77e720bcb7e363545fa52838c0a1
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-05 15:05:15 +0000

    Use closure for ticket status ID

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -4,7 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Ticket::class, function (Faker $faker) {
     return [
-        'status_id' => DB::table('ticket_statuses')->inRandomOrder()->first()->id,
+        'status_id' => function () {
+            return DB::table('ticket_statuses')->inRandomOrder()->first()->id;
+        },
         'user_id' => factory(App\Models\User::class)->create()->id,
         'summary' => $faker->sentence(),
     ];
