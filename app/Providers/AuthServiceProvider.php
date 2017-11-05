25012d82a01a905edeb2dbdc0a3a306c7a78a8cc
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-05 14:32:55 +0000

    Add user policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Models\Ticket;
 use App\Models\TicketPost;
+use App\Models\User;
 use App\Policies\TicketPolicy;
 use App\Policies\TicketPostPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -18,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         Ticket::class => TicketPolicy::class,
         TicketPost::class => TicketPostPolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     /**
