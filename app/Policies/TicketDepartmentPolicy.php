0ce080512c70dbe4125cd2f268fb75116727e719
Neil Farrington <me@neilfarrington.com>
2017-11-09 21:48:25 +0000

    Apply fixes from StyleCI (#83)

--- app/Policies/TicketDepartmentPolicy.php
+++ app/Policies/TicketDepartmentPolicy.php
@@ -2,8 +2,8 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use App\Models\TicketDepartment;
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class TicketDepartmentPolicy
