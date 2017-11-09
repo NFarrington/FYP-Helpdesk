ad095ae8c8add977d735f21c5f59a9256ed812f8
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-09 11:28:04 +0000

    Add password change form to user profile

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -11,6 +11,67 @@
                     @endforeach
                 </div>
             </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Change Password</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('users.update', $user) }}">
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Current Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password"
+                                       value="{{ $password or old('password') }}" required autofocus>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('new_password') ? ' has-error' : '' }}">
+                            <label for="new-password" class="col-md-4 control-label">New Password</label>
+
+                            <div class="col-md-6">
+                                <input id="new-password" type="password" class="form-control" name="new_password" required>
+
+                                @if ($errors->has('new_password'))
+                                    <span class="help-block">
+                                    <strong>{{ $errors->first('new_password') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('new_password_confirmation') ? ' has-error' : '' }}">
+                            <label for="new-password-confirm" class="col-md-4 control-label">Confirm New Password</label>
+                            <div class="col-md-6">
+                                <input id="new-password-confirm" type="password" class="form-control"
+                                       name="new_password_confirmation" required>
+
+                                @if ($errors->has('new_password_confirmation'))
+                                    <span class="help-block">
+                                    <strong>{{ $errors->first('new_password_confirmation') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Change Password
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
