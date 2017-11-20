77142162ae21ac0411e4ca910ee01c24c0e5ec84
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-20 21:59:32 +0000

    Implement article factory

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -1,9 +1,23 @@
 <?php
 
+use App\Models\Article;
 use Faker\Generator as Faker;
 
-$factory->define(App\Models\Article::class, function (Faker $faker) {
+$factory->define(Article::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->sentences(1),
+        'content' => $faker->paragraphs(2),
+    ];
+});
+
+$factory->state(Article::class, 'published', function (Faker $faker) {
+    return [
+        'visible_from' => \Carbon\Carbon::now(),
+    ];
+});
+
+$factory->state(Article::class, 'unpublished', function (Faker $faker) {
+    return [
+        'visible_to' => \Carbon\Carbon::now(),
     ];
 });
