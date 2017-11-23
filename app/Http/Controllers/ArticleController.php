33044e4ee4fe5317f639ba6b48808fae3b60e30b
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-23 12:55:34 +0000

    Implement article creation in controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -24,7 +25,7 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        return view('articles.create');
     }
 
     /**
@@ -35,7 +36,30 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required|string|max:250',
+            'content' => 'required|string|max:60000',
+            'visible_from_date' => 'required_with:visible_from_time|nullable|date_format:Y-m-d',
+            'visible_from_time' => 'required_with:visible_from_date|nullable|date_format:H:i',
+            'visible_to_date' => 'required_with:visible_to_time|nullable|date_format:Y-m-d',
+            'visible_to_time' => 'required_with:visible_to_date|nullable|date_format:H:i',
+        ], [
+            'visible_from_date.date_format' => 'The visible from date does not match the format YYYY-MM-DD.',
+            'visible_from_time.date_format' => 'The visible from time does not match the format HH:MM.',
+            'visible_to_date.date_format' => 'The visible to date does not match the format YYYY-MM-DD.',
+            'visible_to_time.date_format' => 'The visible to time does not match the format HH:MM.',
+        ]);
+
+        $visibleFrom = "{$request->input('visible_from_date')} {$request->input('visible_from_time')}";
+        $visibleTo = "{$request->input('visible_to_date')} {$request->input('visible_to_time')}";
+        $visibility = [
+            'visible_from' => !ctype_space($visibleFrom) ? $visibleFrom : null,
+            'visible_to' => !ctype_space($visibleTo) ? $visibleTo : null,
+        ];
+
+        $article = Article::create(array_merge($request->only('title', 'content') + $visibility));
+
+        return redirect(route('articles.show', $article));
     }
 
     /**
