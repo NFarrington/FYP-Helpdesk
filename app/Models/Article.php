e176cc213e54772b1e1c0d4f80b104a81befce10
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-26 13:00:59 +0000

    Add base attributes to Article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,14 +2,36 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * App\Models\Article
  *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property \Carbon\Carbon|null $visible_from
+ * @property \Carbon\Carbon|null $visible_to
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Article whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Article whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Article whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Article whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Article whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Article whereVisibleFrom($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Article whereVisibleTo($value)
  * @mixin \Eloquent
  */
 class Article extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'content', 'visible_from', 'visible_to'];
+
+    protected $dates = ['visible_from', 'visible_to'];
 }
