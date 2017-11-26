f621004636fe3a310752ebf2859a49ce8b7aa611
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-26 22:39:49 +0000

    Add action buttons to view article page

--- resources/views/articles/view.blade.php
+++ resources/views/articles/view.blade.php
@@ -4,7 +4,17 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Article - {{ $article->title }}</div>
+                <div class="panel-heading">
+                    Article - {{ $article->title }}
+                    <span class="pull-right">
+                        @can('update', $article)
+                            <a class="btn btn-xs btn-warning" href="{{ route('articles.edit', $article) }}">Edit</a>
+                        @endcan
+                        @can('delete', $article)
+                            <delete-resource route="{{ route('articles.destroy', $article) }}"></delete-resource>
+                        @endcan
+                    </span>
+                </div>
                 <div class="panel-body">
                     <p>{{ $article->content }}</p>
                 </div>
