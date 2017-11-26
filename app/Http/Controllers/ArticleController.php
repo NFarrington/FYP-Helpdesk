3011047fb64c4e046f344b67b3dfbb70b3bc3036
Neil Farrington <npf489@student.bham.ac.uk>
2017-11-26 22:41:58 +0000

    Implement editing & refactor ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ArticleController extends Controller
 {
@@ -56,31 +57,11 @@ class ArticleController extends Controller
     public function store(Request $request)
     {
         $this->authorize('create', Article::class);
+        $this->validateResource($request);
 
-        $this->validate($request, [
-            'title' => 'required|string|max:250',
-            'content' => 'required|string|max:60000',
-            'visible_from_date' => 'required_with:visible_from_time|nullable|date_format:Y-m-d',
-            'visible_from_time' => 'required_with:visible_from_date|nullable|date_format:H:i',
-            'visible_to_date' => 'required_with:visible_to_time|nullable|date_format:Y-m-d',
-            'visible_to_time' => 'required_with:visible_to_date|nullable|date_format:H:i',
-        ], [
-            'visible_from_date.date_format' => 'The visible from date does not match the format YYYY-MM-DD.',
-            'visible_from_time.date_format' => 'The visible from time does not match the format HH:MM.',
-            'visible_to_date.date_format' => 'The visible to date does not match the format YYYY-MM-DD.',
-            'visible_to_time.date_format' => 'The visible to time does not match the format HH:MM.',
-        ]);
+        $article = Article::create($this->getAttributesFromInput($request));
 
-        $visibleFrom = "{$request->input('visible_from_date')} {$request->input('visible_from_time')}";
-        $visibleTo = "{$request->input('visible_to_date')} {$request->input('visible_to_time')}";
-        $visibility = [
-            'visible_from' => !ctype_space($visibleFrom) ? $visibleFrom : null,
-            'visible_to' => !ctype_space($visibleTo) ? $visibleTo : null,
-        ];
-
-        $article = Article::create(array_merge($request->only('title', 'content') + $visibility));
-
-        return redirect(route('articles.show', $article));
+        return redirect(route('articles.show', $article))->with('status', 'Article created successfully.');
     }
 
     /**
@@ -104,7 +85,11 @@ class ArticleController extends Controller
      */
     public function edit(Article $article)
     {
-        //
+        $this->authorize('update', $article);
+
+        Session::flashInput($article->toArray());
+
+        return view('articles.edit')->with('article', $article);
     }
 
     /**
@@ -116,7 +101,12 @@ class ArticleController extends Controller
      */
     public function update(Request $request, Article $article)
     {
-        //
+        $this->authorize('update', $article);
+        $this->validateResource($request);
+
+        $article->update($this->getAttributesFromInput($request));
+
+        return redirect(route('articles.show', $article))->with('status', 'Article updated successfully.');
     }
 
     /**
@@ -132,5 +122,34 @@ class ArticleController extends Controller
         $article->delete();
 
         return redirect(route('articles.index'))->with('status', 'Article deleted successfully.');
+    }
+
+    protected function validateResource(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:250',
+            'content' => 'required|string|max:60000',
+            'visible_from_date' => 'required_with:visible_from_time|nullable|date_format:Y-m-d',
+            'visible_from_time' => 'required_with:visible_from_date|nullable|date_format:H:i',
+            'visible_to_date' => 'required_with:visible_to_time|nullable|date_format:Y-m-d',
+            'visible_to_time' => 'required_with:visible_to_date|nullable|date_format:H:i',
+        ], [
+            'visible_from_date.date_format' => 'The visible from date does not match the format YYYY-MM-DD.',
+            'visible_from_time.date_format' => 'The visible from time does not match the format HH:MM.',
+            'visible_to_date.date_format' => 'The visible to date does not match the format YYYY-MM-DD.',
+            'visible_to_time.date_format' => 'The visible to time does not match the format HH:MM.',
+        ]);
+    }
+
+    protected function getAttributesFromInput(Request $request)
+    {
+        $visibleFrom = "{$request->input('visible_from_date')} {$request->input('visible_from_time')}";
+        $visibleTo = "{$request->input('visible_to_date')} {$request->input('visible_to_time')}";
+        $visibility = [
+            'visible_from' => !ctype_space($visibleFrom) ? $visibleFrom : null,
+            'visible_to' => !ctype_space($visibleTo) ? $visibleTo : null,
+        ];
+
+        return array_merge($request->only('title', 'content') + $visibility);
     }
 }
