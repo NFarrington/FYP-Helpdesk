d267a5c472634fbd6db95c87d67bcf73c4987091
Neil Farrington <me@neilfarrington.com>
2018-01-06 13:10:35 +0000

    Apply fixes from StyleCI (#102)

--- app/Http/Middleware/VerifyRole.php
+++ app/Http/Middleware/VerifyRole.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Role;
 use Closure;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\UnauthorizedException;
 
 class VerifyRole
 {
