ad841ffdc2bbefffb648ea57e84d7bf2f14ba7c9
Neil Farrington <npf489@student.bham.ac.uk>
2018-01-08 23:30:25 +0000

    Add description to RoleFactory

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -6,5 +6,6 @@ $factory->define(\App\Models\Role::class, function (Faker $faker) {
     return [
         'key' => str_random(),
         'name' => $faker->words(3, true).' - '.str_random(),
+        'description' => $faker->sentence(),
     ];
 });
