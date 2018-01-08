6de78580e9804f411f58da55151e338e57d43871
Neil Farrington <npf489@student.bham.ac.uk>
2018-01-08 23:32:42 +0000

    Add permission lang file

---
+++ resources/lang/en/permission.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'key' => [
+        'id' => 'Permission ID',
+        'key' => 'Key',
+        'default' => 'Default',
+        'name' => 'Name',
+        'description' => 'Description',
+
+        'roles' => 'Roles',
+    ],
+
+    'updated' => 'Permission updated.',
+
+];
