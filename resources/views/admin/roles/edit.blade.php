c3c726c3617008271c9727fb235cd23803e37492
Neil Farrington <npf489@student.bham.ac.uk>
2018-01-08 23:32:10 +0000

    Add role views

---
+++ resources/views/admin/roles/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Update Role</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('admin.roles.update', $role) }}">
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}
+
+                        @include('admin.roles.includes.form')
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Update Role
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
