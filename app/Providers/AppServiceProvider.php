f1fd7cec170c22ab90519cb6a81f1744805a6c1f
Neil Farrington <npf489@student.bham.ac.uk>
2018-01-12 17:08:31 +0000

    Bind Facebook provider to service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use League\OAuth2\Client\Provider\Facebook;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(Facebook::class, function ($app) {
+            return new Facebook([
+                'clientId'          => config('services.facebook.id'),
+                'clientSecret'      => config('services.facebook.secret'),
+                'redirectUri'       => route('login.facebook.callback'),
+                'graphApiVersion'   => config('services.facebook.version'),
+            ]);
+        });
     }
 }
