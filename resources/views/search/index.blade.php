369d968bbc1641f0114aaa42fb9a05df733077c7
Neil Farrington <npf489@student.bham.ac.uk>
2018-01-28 11:39:53 +0000

    Add search views

---
+++ resources/views/search/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Tickets</div>
+                @include('tickets.includes.ticket-table', ['tickets' => $tickets])
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Articles</div>
+                @include('articles.includes.table', ['articles' => $articles])
+            </div>
+
+            @if(Auth::user()->hasRole(\App\Models\Role::admin()))
+                <div class="panel panel-default">
+                    <div class="panel-heading">Users</div>
+                    @include('admin.users.includes.table', ['users' => $users])
+                </div>
+            @endif
+        </div>
+    </div>
+@endsection
