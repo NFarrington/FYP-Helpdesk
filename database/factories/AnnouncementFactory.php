30755649ef7e9f98495efb00138cbb753287f32c
Neil Farrington <npf489@student.bham.ac.uk>
2018-02-10 10:48:03 +0000

    Add announcement model and factory

---
+++ database/factories/AnnouncementFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Announcement::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph,
+        'status' => rand(0, 2),
+    ];
+});
+
+$factory->state(App\Models\Announcement::class, 'active', function (Faker $faker) {
+    return [
+        'status' => \App\Models\Announcement::STATUS_ACTIVE,
+    ];
+});
+
+$factory->state(App\Models\Announcement::class, 'published', function (Faker $faker) {
+    return [
+        'status' => \App\Models\Announcement::STATUS_PUBLISHED,
+    ];
+});
+
+$factory->state(App\Models\Announcement::class, 'unpublished', function (Faker $faker) {
+    return [
+        'status' => \App\Models\Announcement::STATUS_UNPUBLISHED,
+    ];
+});
