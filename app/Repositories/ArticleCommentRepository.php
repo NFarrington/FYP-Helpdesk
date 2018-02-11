cbb8f46bb0dfc503dd066be7c3564e42e464cfd7
Neil Farrington <npf489@student.bham.ac.uk>
2018-02-11 19:08:19 +0000

    Add default properties to ArticleCommentRepository

--- app/Repositories/ArticleCommentRepository.php
+++ app/Repositories/ArticleCommentRepository.php
@@ -3,7 +3,31 @@
 namespace App\Repositories;
 
 
+use App\Models\ArticleComment;
+
 class ArticleCommentRepository extends Repository
 {
-    //
+    /**
+     * The order to sort the results by.
+     *
+     * @var string
+     */
+    protected $sortOrder = ['created_at', 'DESC'];
+
+    /**
+     * The model.
+     *
+     * @var ArticleComment
+     */
+    protected $model;
+
+    /**
+     * EloquentRepository constructor.
+     *
+     * @param ArticleComment $model
+     */
+    public function __construct(ArticleComment $model)
+    {
+        $this->model = $model;
+    }
 }
