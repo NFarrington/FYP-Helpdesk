4161010ec03867f588c0f9d32126c227d2005195
Neil Farrington <npf489@student.bham.ac.uk>
2018-02-11 19:01:03 +0000

    Refactor ArticleCommentController to service

---
+++ app/Services/ArticleCommentService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Models\Article;
+use App\Models\ArticleComment;
+use App\Models\User;
+use App\Repositories\ArticleCommentRepository;
+
+class ArticleCommentService extends Service
+{
+    /**
+     * The repository.
+     *
+     * @var ArticleCommentRepository
+     */
+    protected $repository;
+
+    /**
+     * Initialise the service.
+     *
+     * @param ArticleCommentRepository $repository
+     */
+    public function __construct(ArticleCommentRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Create a new announcement.
+     *
+     * @param array $attributes
+     * @param Article $article
+     * @param User $user
+     * @return ArticleComment
+     */
+    public function create(array $attributes, Article $article, User $user)
+    {
+        $articleComment = new ArticleComment($attributes);
+        $articleComment->article()->associate($article);
+        $articleComment->user()->associate($user);
+        $articleComment->save();
+
+        return $articleComment;
+    }
+}
