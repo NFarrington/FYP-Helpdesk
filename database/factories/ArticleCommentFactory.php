8e1a59978013431990ed40fcf46295088727bfc6
Neil Farrington <npf489@student.bham.ac.uk>
2018-02-11 19:00:42 +0000

    Include relations in ArticleComment factory

--- database/factories/ArticleCommentFactory.php
+++ database/factories/ArticleCommentFactory.php
@@ -5,5 +5,11 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\ArticleComment::class, function (Faker $faker) {
     return [
         'content' => $faker->sentence,
+        'article_id' => function () {
+            return factory(App\Models\Article::class)->create()->id;
+        },
+        'user_id' => function () {
+            return factory(App\Models\User::class)->create()->id;
+        },
     ];
 });
