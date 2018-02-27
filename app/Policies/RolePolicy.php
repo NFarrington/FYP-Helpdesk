e187f0b044794d206085ebaf8e45963895516b12
Neil Farrington <npf489@student.bham.ac.uk>
2018-02-27 08:23:40 +0000

    Add user/role/permission policies

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -11,6 +11,30 @@ class RolePolicy
     use HandlesAuthorization;
 
     /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User $user
+     * @param Role|null $role
+     * @return mixed
+     */
+    public function view(User $user, Role $role = null)
+    {
+        return $user->hasRole(Role::admin());
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User $user
+     * @param Role|null $role
+     * @return mixed
+     */
+    public function update(User $user, Role $role = null)
+    {
+        return $user->hasRole(Role::admin());
+    }
+
+    /**
      * Determine whether the user has the role.
      *
      * @param  \App\Models\User  $user
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,15 +5,19 @@ namespace App\Providers;
 use App\Models\Announcement;
 use App\Models\Article;
 use App\Models\Department;
+use App\Models\Permission;
 use App\Models\Role;
 use App\Models\Ticket;
 use App\Models\TicketPost;
+use App\Models\User;
 use App\Policies\AnnouncementPolicy;
 use App\Policies\ArticlePolicy;
 use App\Policies\DepartmentPolicy;
+use App\Policies\PermissionPolicy;
 use App\Policies\RolePolicy;
 use App\Policies\TicketPolicy;
 use App\Policies\TicketPostPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -28,9 +32,11 @@ class AuthServiceProvider extends ServiceProvider
         Announcement::class => AnnouncementPolicy::class,
         Article::class => ArticlePolicy::class,
         Department::class => DepartmentPolicy::class,
+        Permission::class => PermissionPolicy::class,
         Role::class => RolePolicy::class,
         Ticket::class => TicketPolicy::class,
         TicketPost::class => TicketPostPolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     /**
