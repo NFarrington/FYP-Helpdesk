7c924737a1d3791faabb18ef37b5eb4ff39fb54d
Neil Farrington <npf489@student.bham.ac.uk>
2018-03-18 20:16:25 +0000

    Fix article creation button text

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -13,7 +13,7 @@
                 <div class="form-group">
                     <div class="col-md-8 col-md-offset-4">
                         <button type="submit" class="btn btn-primary">
-                            Create Ticket
+                            Create Article
                         </button>
                     </div>
                 </div>
