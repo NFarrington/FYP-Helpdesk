5ce360cf90004ef095b1d42e97cc6d5d5de155c1
Neil Farrington <npf489@student.bham.ac.uk>
2018-03-20 23:25:34 +0000

    Fix fragment.js to remove only matching fragments

--- resources/assets/js/fragment.js
+++ resources/assets/js/fragment.js
@@ -2,6 +2,6 @@
 /**
  * Remove hash fragments when redirected from OAuth.
  */
-if (window.location.hash.match(/^(#_=_)?$/)) {
+if (window.location.href.indexOf('#') !== -1 && window.location.hash.match(/^$|^#$|^#_=_$/)) {
     history.pushState('', document.title, window.location.pathname + window.location.search);
 }
