83000533faaba4bea4cd2dd923ac341f4cfca121
Neil Farrington <npf489@student.bham.ac.uk>
2018-03-20 13:23:27 +0000

    Update factory field lengths

--- database/factories/SlackWebhookFactory.php
+++ database/factories/SlackWebhookFactory.php
@@ -7,7 +7,7 @@ $factory->define(\App\Models\SlackWebhook::class, function (Faker $faker) {
         'user_id' => function () {
             return factory(App\Models\User::class)->create()->id;
         },
-        'name' => $faker->sentence,
+        'name' => str_limit($faker->sentence, 50, ''),
         'uri' => sprintf('https://hooks.slack.com/services/%s/%s/%s', str_random(8), str_random(9), str_random(24)),
         'recipient' => '#general',
     ];
