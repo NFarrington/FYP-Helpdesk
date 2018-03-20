409af5999920ca217f5e96d65cbe627531cd3c8d
Neil Farrington <npf489@student.bham.ac.uk>
2018-03-20 13:18:18 +0000

    Update long field wrapping in tables

--- resources/views/admin/permissions/includes/table.blade.php
+++ resources/views/admin/permissions/includes/table.blade.php
@@ -1,33 +1,34 @@
-
 @if($permissions->isNotEmpty())
-    <table class="table table-hover {{ $permissions->hasPages() ? 'table-bordered-bottom' : '' }}">
-        <tr>
-            <th>{{ __('permission.key.id') }}</th>
-            <th>{{ __('permission.key.key') }}</th>
-            <th>{{ __('permission.key.name') }}</th>
-            <th>{{ __('permission.key.description') }}</th>
-            <th>{{ __('permission.key.roles') }}</th>
-            <th></th>
-        </tr>
-        @foreach($permissions as $permission)
+    <div class="table-responsive">
+        <table class="table table-hover {{ $permissions->hasPages() ? 'table-bordered-bottom' : '' }}">
             <tr>
-                <td>#{{ $permission->id }}</td>
-                <td>{{ $permission->key }}</td>
-                <td>{{ $permission->name }}</td>
-                <td>{{ $permission->description }}</td>
-                <td>
-                    <ul class="list-unstyled">
-                        @foreach($permission->roles as $role)
-                            <li>
-                                <a href="{{ route('admin.roles.edit', $role) }}">{{ $role->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </td>
-                <td><a href="{{ route('admin.permissions.edit', $permission) }}">Edit</a></td>
+                <th>{{ __('permission.key.id') }}</th>
+                <th>{{ __('permission.key.key') }}</th>
+                <th>{{ __('permission.key.name') }}</th>
+                <th>{{ __('permission.key.description') }}</th>
+                <th>{{ __('permission.key.roles') }}</th>
+                <th></th>
             </tr>
-        @endforeach
-    </table>
+            @foreach($permissions as $permission)
+                <tr>
+                    <td>#{{ $permission->id }}</td>
+                    <td>{{ $permission->key }}</td>
+                    <td>{{ $permission->name }}</td>
+                    <td class="wrap">{{ $permission->description }}</td>
+                    <td>
+                        <ul class="list-unstyled">
+                            @foreach($permission->roles as $role)
+                                <li>
+                                    <a href="{{ route('admin.roles.edit', $role) }}">{{ $role->name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </td>
+                    <td><a href="{{ route('admin.permissions.edit', $permission) }}">Edit</a></td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 
     <div class="text-center">{{ $permissions->links() }}</div>
 @else
