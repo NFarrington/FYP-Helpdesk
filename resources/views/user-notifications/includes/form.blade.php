afaffa73111e93bb1379e0c97724cf70fe88203a
Neil Farrington <npf489@student.bham.ac.uk>
2018-03-20 13:18:01 +0000

    Update notification settings form styling

--- resources/views/user-notifications/includes/form.blade.php
+++ resources/views/user-notifications/includes/form.blade.php
@@ -1,8 +1,8 @@
 <h5>User Notifications</h5>
 
 <div class="form-group">
-    <label class="col-md-offset-4 col-md-3">Email</label>
-    <label class="col-md-3">Slack</label>
+    <label class="col-md-offset-4 col-md-3 col-xs-offset-5 col-xs-5">Email</label>
+    <label class="col-md-3 col-xs-2">Slack</label>
 </div>
 
 @include('user-notifications.includes.notification-form', ['notifications' => \App\Services\NotificationService::USER_NOTIFICATIONS])
