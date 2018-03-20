98c1bc273632d3e1d265ed79718774e64e3afb24
Neil Farrington <npf489@student.bham.ac.uk>
2018-03-20 19:23:44 +0000

    Add breadcrumbs to page title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} - {{ title_case(str_replace('/', ' / ', Request::decodedPath())) }}</title>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
